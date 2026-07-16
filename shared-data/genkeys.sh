9dd098ebb1c1785b6233328a58f39d5f5f1f3988
alichian <alistar.ottochian@yahoo.fr>
2026-07-16 10:20:27 +0200

    Put a generator script for brand new keys

--- shared-data/genkeys.sh
+++ shared-data/genkeys.sh
@@ -3,7 +3,11 @@
 ##This script is used to regenerate the security keys for sshd and munge
 
 #for ssh
-ssh-keygen -t ed25519f -f ./slurm-rsa
+ssh-keygen -t ed25519 -f ./slurm-rsa -N ""
 
 #for munge
 dd if=/dev/urandom bs=1 count=1024 of=./munge.key
+
+## protect them
+chmod 400 slurm-rsa munge.key
+
